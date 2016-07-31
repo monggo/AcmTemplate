ba0fc1071786cbddf076a15e49e2e9b40c65ce58
monggo <monggo1995@gmail.com>
2016-07-31 14:15:02 +0800

    update

--- Graph/二分图的最大权值匹配.cpp
+++ Graph/二分图的最大权值匹配.cpp
@@ -1,53 +1,62 @@
-const int inf = 1<<30;
-int nx, ny;   // nx,ny分别为x点集y点集的个数
-int link[M], lx[M], ly[M], slack[M];    // lx,ly为顶标
-int visx[M], visy[M], w[M][M];
+/*  KM算法
+ *   复杂度O(nx*nx*ny)
+ *  求最大权匹配
+ *   若求最小权匹配，可将权值取相反数，结果取相反数
+ *  点的编号从0开始
+ */
+const int N = 310;
+const int INF = 0x3f3f3f3f;
+int nx,ny; //两边的点数
+int g[N][N]; //二分图描述
+int linker[N],lx[N],ly[N]; //y中各点匹配状态，x,y中的点标号
+int slack[N];
+bool visx[N],visy[N];
 
-int DFS(int x) {
-    visx[x] = 1;
-    for (int y = 1; y <= ny; y++) {
-        if (visy[y]) continue;
-        int t = lx[x]+ly[y]-w[x][y];
-        if (t == 0) {
-            visy[y] = 1;
-            if (link[y] == -1||DFS(link[y])) {
-                link[y] = x;
-                return 1;
+bool DFS(int x) {
+    visx[x] = true;
+    for(int y = 0; y < ny; y++) {
+        if(visy[y]) continue;
+        int tmp = lx[x]+ly[y]-g[x][y];
+        if(tmp == 0) {
+            visy[y] = true;
+            if(linker[y]==-1 || DFS(linker[y])) {
+                linker[y] = x;
+                return true;
             }
         }
-        else if (slack[y] > t)
-            slack[y] = t;
+        else if(slack[y] > tmp)
+            slack[y] = tmp;
     }
-    return 0;
+    return false;
 }
 int KM() {
-    int i, j;
-    memset(link, -1, sizeof(link));
+    memset(linker, -1, sizeof(linker));
     memset(ly, 0, sizeof(ly));
-    for (i = 1; i <= nx; i++)
-        for (j = 1, lx[i] = -inf; j <= ny; j++)
-            if (w[i][j] > lx[i]) lx[i] = w[i][j];
-
-    for (int x = 1; x <= nx; x++) {
-        for (i = 1; i <= ny; i++)
-            slack[i] = inf;
-        while (1) {
-            memset(visx, 0, sizeof(visx));
-            memset(visy, 0, sizeof(visy));
-            if (DFS(x)) break;
-            int d = inf;
-            for (i = 1; i <= ny; i++)
-                if (!visy[i]&& d>slack[i]) d = slack[i];
-            for (i = 1; i <= nx; i++)
-                if (visx[i]) lx[i] -= d;
-            for (i = 1; i <= ny; i++)
-                if (visy[i]) ly[i] += d;
+    for(int i = 0; i < nx; i++) {
+        lx[i] = -INF;
+        for(int j = 0; j < ny; j++)
+            if(g[i][j] > lx[i]) lx[i] = g[i][j];
+    }
+    for(int x = 0; x < nx; x++) {
+        for(int i = 0; i < ny; i++) slack[i] = INF;
+        while(true) {
+            memset(visx, false, sizeof(visx));
+            memset(visy, false, sizeof(visy));
+            if(DFS(x)) break;
+            int d = INF;
+            for(int i = 0; i < ny; i++)
+                if(!visy[i] && d>slack[i]) d = slack[i];
+            for(int i = 0; i < nx; i++)
+                if(visx[i]) lx[i] -= d;
+            for(int i = 0; i < ny; i++) {
+                if(visy[i]) ly[i] += d;
                 else slack[i] -= d;
+            }
         }
     }
     int res = 0;
-    for (i = 1; i <= ny; i++)
-        if (link[i] > -1)
-            res += w[link[i]][i];
+    for(int i = 0; i < ny; i++)
+        if(linker[i] != -1) res += g[linker[i]][i];
+
     return res;
 }
